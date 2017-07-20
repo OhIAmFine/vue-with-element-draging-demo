fd0a5dd3e734e4b250cd5c1c59b7973df8bbf256
OhIAmFine <845782625@qq.com>
2017-07-20 09:30:53 +0800

    [+]增加模块demo

---
+++ src/App.vue
@@ -0,0 +1,50 @@
+<template>
+  <div id="app">
+    <!-- <img src="./assets/logo.png"> -->
+    <!-- <h1>{{ msg }}</h1> -->
+    <!-- <el-button @click.native="startHacking">Let's do it</el-button> -->
+    <!-- <buttonclick></buttonclick> -->
+    <!-- <cloumn></cloumn> -->
+    <!-- <collapse></collapse> -->
+    <!-- <carousel></carousel> -->
+    <!-- <card></card> -->
+    <sidebar></sidebar>
+    <drag></drag>
+  </div>
+</template>
+
+<script>
+import buttonclick from './Buttonclick.vue'
+import cloumn from './cloumn.vue'
+import collapse from './collapse.vue'
+import carousel from './carousel.vue'
+import card from './card.vue'
+import sidebar from './sidebar.vue'
+import drag from './drag.vue'
+export default {
+  data () {
+    return {
+      msg: 'Use Vue 2.0 Today!'
+    }
+  },
+  components : {buttonclick,cloumn,collapse,carousel,card,sidebar,drag},
+  methods: {
+    startHacking () {
+      // this.$notify({
+      //   title: 'It Works',
+      //   message: 'We have laid the groundwork for you. Now it\'s your time to build something epic!',
+      //   duration: 6000
+      // })
+      alert('haha')
+    }
+  }
+}
+</script>
+
+<style>
+@import url("//unpkg.com/element-ui@1.3.7/lib/theme-default/index.css");
+body {
+
+  font-family: Helvetica, sans-serif;
+}
+</style>
---
+++ src/sidebar.vue
@@ -0,0 +1,60 @@
+
+<template>
+  <div>
+    <el-row class="tac">
+  <el-col :span="8" v-on:click="clickTest">
+
+    <el-menu  draggable='true' v-drag='dragstart'  :style="style"
+
+     default-active="2" class="el-menu-vertical-demo" @open="handleOpen" @close="handleClose" theme="dark" >
+      <el-submenu index="1">
+        <template slot="title">导航一</template>
+        <el-menu-item-group title="分组一">
+          <el-menu-item index="1-1">选项1</el-menu-item>
+          <el-menu-item index="1-2">选项2</el-menu-item>
+        </el-menu-item-group>
+        <el-menu-item-group title="分组2">
+          <el-menu-item index="1-3">选项3</el-menu-item>
+        </el-menu-item-group>
+        <el-submenu index="1-4">
+          <template slot="title">选项4</template>
+          <el-menu-item index="1-4-1">选项1</el-menu-item>
+        </el-submenu>
+      </el-submenu>
+      <el-menu-item index="2">导航二</el-menu-item>
+      <el-menu-item index="3">导航三</el-menu-item>
+    </el-menu>
+  </el-col>
+</el-row>
+  </div>
+</template>
+
+<script>
+
+export default {
+    methods: {
+      handleOpen(key, keyPath) {
+        console.log(key, keyPath);
+      },
+      handleClose(key, keyPath) {
+        console.log(key, keyPath);
+      },
+      dragstart(val){
+        this.val = val;
+      },
+      clickTest(){
+        alert('clickTest')
+      }
+    }
+  }
+
+</script>
+<style >
+
+  .el-menu-vertical-demo{
+    font-size:30px;
+  }
+  .el-menu{
+    border-radius: 30px;
+  }
+</style>
